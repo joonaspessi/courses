f6fdcb8a1d472bdc6d851d013b6077ecab1a35ff
Joonas Pessi <joonas.pessi@gmail.com>
2018-02-17 19:41:53 +0200

    Week7 first ex

---
+++ SGN-41007/Week7/ex7_3.py
@@ -0,0 +1,23 @@
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+from keras.layers import Dense, Flatten
+
+# Disable annoying tensorflow cpu support warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+if __name__ == "__main__":
+
+    base_model = VGG16(include_top=False, weights="imagenet", input_shape=(64, 64, 3))
+
+    w = base_model.output
+
+    w = Flatten()(w)
+
+    w = Dense(100, activation="relu")(w)
+
+    output = Dense(2, activation="sigmoid")(w)
+
+    model = Model(inputs=[base_model.input], outputs=[output])
+
+    model.summary()
