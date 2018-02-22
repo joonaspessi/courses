d68dd6cddcc9e29b88a9c640aba6c9ca1b0f0946
Joonas Pessi <joonas.pessi@gmail.com>
2018-02-22 20:11:27 +0200

    Week 7 ex5

--- SGN-41007/Week7/ex7_45.py
+++ SGN-41007/Week7/ex7_45.py
@@ -3,6 +3,7 @@ from scipy.io import loadmat
 from sklearn.feature_selection import RFECV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 
 
@@ -28,3 +29,22 @@ if __name__ == "__main__":
     y_pred = rfe.predict(X_test)
     score = accuracy_score(y_test, y_pred)
     print("accuracy:", score * 100, "%")
+
+    # ex5
+    c_range = np.linspace(1e-5, 100, num=10)
+    for C in c_range:
+        clf = LogisticRegression(penalty="l1", C=C)
+        scores = cross_val_score(clf, X_train, y_train, cv=10)
+        print("C: %.5f" % C)
+        print("---------")
+        print("CV Accuracy (10-fold): %.2f +- %.2f" % (np.mean(scores), np.std(scores)))
+
+        model = LogisticRegression(penalty="l1", C=C)
+        model.fit(X_train, y_train)
+
+        a = np.where(model.coef_[0] > 1e-5)
+        print("Selected coefficients: %d/%d" % (len(a[0]), len(model.coef_[0])))
+
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_train, y_pred)
+        print("Test Accuracy: %.2f" % accuracy)
