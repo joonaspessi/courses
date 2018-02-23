85a136ad4bbd629c33efb97f25b215954bd67326
Joonas Pessi <joonas.pessi@futurice.com>
2018-02-23 14:14:23 +0200

    Remove empty lines

--- SGN-41007/Week7/ex7_3.py
+++ SGN-41007/Week7/ex7_3.py
@@ -9,15 +9,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 if __name__ == "__main__":
 
     base_model = VGG16(include_top=False, weights="imagenet", input_shape=(64, 64, 3))
-
     w = base_model.output
-
     w = Flatten()(w)
-
     w = Dense(100, activation="relu")(w)
-
     output = Dense(2, activation="sigmoid")(w)
-
     model = Model(inputs=[base_model.input], outputs=[output])
-
     model.summary()
