e563a06c02147d629ee188c8afc26a8fdecc0eea
Joonas Pessi <joonas.pessi@futurice.com>
2018-04-07 11:41:11 +0300

    Update week 7 ex 5.9

--- SGN-11000/ex7/ex5_9.m
+++ SGN-11000/ex7/ex5_9.m
@@ -1,22 +1,27 @@
 clear();
 
-Fs_2 = 44100 / 2;
+Fs = 44100;
+Fs_2 = Fs / 2;
 passband = 18000 / Fs_2;
-N = 98;
 Wn = passband;
 type = 'high';
 
 % Päästökaistan maksimivärähtely 0.1 dB
 % Estokaistan minimivaimennus 30 dB
 % => opintomoniste s.84 taulukko
-% => Hanning
+% => Hanning 
+N_candidate = 3.1 / (3000 / Fs);
 
-window = hanning(N+1)
+% Pyöristetään ylöspäin seuraavaan parittomaan
+N = 2*ceil(N_candidate/2)+1;
+window = hanning(N);
+d = fir1(N - 1, passband, type, window);
 
-d = fir1 (N, passband, type, window);
-
+figure('Name', 'impz')
 impz(d)
-figure
+
+figure('Name', 'freq')
 freqz(d)
-figure
+
+figure('Name', 'zplane')
 zplane(d)
